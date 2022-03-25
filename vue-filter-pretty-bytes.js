8161aad46e70166bdf71e31f6d3593595fde0a7e
Adam Jimenez <adam.jimenez@gmail.com>
2022-03-25 14:53:01 +0000

    fixes #5

--- vue-filter-pretty-bytes.js
+++ vue-filter-pretty-bytes.js
@@ -16,7 +16,7 @@
      Vue.filter('prettyBytes', function (bytes, decimals, kib, maxunit) {
        kib = kib || false
        if (bytes === 0) return '0 Bytes'
-       if (isNaN(parseFloat(bytes)) && !isFinite(bytes)) return 'Not an number'
+       if (isNaN(parseFloat(bytes)) && !isFinite(bytes)) return ''
        const k = kib ? 1024 : 1000
        const dm = decimals != null && !isNaN(decimals) && decimals >= 0 ? decimals : 2
        const sizes = kib ? ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB', 'BiB'] : ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB', 'BB']
